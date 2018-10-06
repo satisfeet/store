f450af2a6e3ea96faa7ce4f56cba3dd28a82d6c0
Bodo Kaiser <i@bodokaiser.io>
2018-10-06 21:09:16 +0200

    created static clone for demo

--- app/models/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-
-  require('./product')(app);
-
-  require('./order')(app);
-
-};
--- opt/test/bin/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var app = express();
-
-require('../../../lib/config')(app);
-
-require('../../../lib/models')(app);
-
-module.exports = app;
